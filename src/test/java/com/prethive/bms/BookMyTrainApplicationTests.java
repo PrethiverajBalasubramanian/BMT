bac4cc0ffe8553c6a2a312732baa621cfe472928
prethiveraj.bm <prethiveraj.bm@zohocorp.com>
2024-04-01 17:08:56 +0530

    initial commit

---
+++ src/test/java/com/prethive/bms/BookMyTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prethive.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
